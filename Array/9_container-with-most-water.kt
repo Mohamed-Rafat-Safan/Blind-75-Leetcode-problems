9faaf106a2eec0dac34acd844390ade899ae665d
Mohamed Ra'afat <76782050+Mohamed-Rafat-Safan@users.noreply.github.com>
2023-07-22 22:09:27 +0300

    Create 9_container-with-most-water.kt

---
+++ Array/9_container-with-most-water.kt
@@ -0,0 +1,16 @@
+import kotlin.math.max
+import kotlin.math.min
+
+class Solution {
+    fun maxArea(height: IntArray): Int {
+        var l = 0
+        var r = height.size - 1
+        var area = 0
+        while (l != r) {
+            area = max(area, min(height[l], height[r]) * (r - l))
+            if (height[l] > height[r]) {r -= 1}
+            else { l += 1 }
+        }
+        return area
+    }
+}
