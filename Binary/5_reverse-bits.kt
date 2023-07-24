e8f8db5e12d5b5e6972a2a32ebc5bc83f74e0a7d
Mohamed Ra'afat <76782050+Mohamed-Rafat-Safan@users.noreply.github.com>
2023-07-24 18:02:26 +0300

    Create 5_reverse-bits.kt

---
+++ Binary/5_reverse-bits.kt
@@ -0,0 +1,10 @@
+class Solution {
+    // you need treat n as an unsigned value
+fun reverseBits(n: Int): Int {
+    var result = 0
+    for (i in 0 until 32) {
+        result = result.shl(1) or ((n.shr(i)) and 1)
+    }
+    return result
+}
+}
